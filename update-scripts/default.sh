2c8f8db14fb02367abfb5cd42453f74a00832b3b
Jingbei Li <i@jingbei.li>
2022-08-19 15:38:11 +0800

    ensure newver is greater fix #15

--- update-scripts/default.sh
+++ update-scripts/default.sh
@@ -10,6 +10,7 @@ cd ${pkgbase}
 
 oldver=$(grep -P '^pkgver' PKGBUILD | cut -d= -f2)
 [ "${oldver}" = "${newver}" ] && echo "${pkgbase} is already at ${newver}" && exit 0
+[ $(vercmp "${oldver}" = "${newver}") -eq 1 ] && echo "The oldver ${oldver} is greater than newver ${newver}." && exit 1
 
 sed "s/^pkgver=.*$/pkgver=${newver}/" -i PKGBUILD
 sed "s/^pkgrel=.*$/pkgrel=1/" -i PKGBUILD
