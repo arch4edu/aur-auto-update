ca1e263aa149a3ed61bc64ef88e5a53245581473
Jingbei Li <i@jingbei.li>
2023-05-29 16:24:16 +0800

    call nvtake in the push job

--- nvchecker.py
+++ nvchecker.py
@@ -8,20 +8,18 @@ from pathlib import Path
 import toml
 import yaml
 
-nvchecker_toml = {}
+nvchecker_toml = toml.load("config/__config__.toml")
+if not 'GITHUB_TOKEN' in os.environ and 'keyfile' in nvchecker_toml['__config__']:
+    del nvchecker_toml['__config__']['keyfile']
 
-for i in Path("config").rglob("*.yaml"):
+for i in [Path(j) for j in sys.argv[1:]] if len(sys.argv) > 1 else Path("config").rglob("*.yaml"):
     if i.stem in ["example"]:
         continue
     try:
         with open(i) as f:
             config = yaml.safe_load(f)
-            if i.stem == "__config__":
-                if not 'GITHUB_TOKEN' in os.environ:
-                    del config['keyfile']
-            else:
-                config = config["nvchecker"]
-                config["user_agent"] = "nvchecker"
+            config = config["nvchecker"]
+            config["user_agent"] = "nvchecker"
             nvchecker_toml[i.stem] = config
         print("Loaded", i)
     except:
